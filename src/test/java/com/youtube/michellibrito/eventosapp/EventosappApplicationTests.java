2a38fd7586d027fd3a4bbb82797960b51b6dd996
Wagner Galvão <galvao.wagner@outlook.com.br>
2022-07-24 18:12:53 -0300

    aula 01 alteracao do package name

---
+++ src/test/java/com/youtube/michellibrito/eventosapp/EventosappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youtube.michellibrito.eventosapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventosappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
